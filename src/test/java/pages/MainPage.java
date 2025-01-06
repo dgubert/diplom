92e20b56075f950070015c422bf2f68ba87c8fc7
dgubert <dgubert86@gmail.com>
2025-01-07 02:43:26 +0300

    fix

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class MainPage {
 
     SelenideElement linkCity = $(".header-upper-region-control__text-overflow"),
-            chooseCityForm = $(".loader-wrapper");
+            chooseCityForm = $(byText("Ваш населенный пункт")).parent().parent();
 
     @Step("Изменяем регион на {0}")
     public void changeCity(String city) {
